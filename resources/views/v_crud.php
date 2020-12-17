54595cd068e5eef1a0ebec7402b019e5ed07aad6
Gabriel-Braga-03 <gbam030303@gmail.com>
2020-12-17 08:36:14 -0300

    Commit 17-12-20

---
+++ resources/views/v_crud.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Login</title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="./css/v_login.css">
+    </head>
+    <body>
+        <!-- Divisória de Envolpe -->
+        <div id="wrap">
+
+        </div>
+    </body>
+</html>
